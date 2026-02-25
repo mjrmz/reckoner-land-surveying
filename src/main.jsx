c1f221d2f80cedbe091cd945889f6938dc7c07a6
mjrmz <mjramirez33363@gmail.com>
2026-02-25 21:34:18 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import "@fontsource/play"; // Defaults to weight 400
+// Optional: import specific weights/styles
+import "@fontsource/play/700.css"; // Example for bold (weight 700)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
